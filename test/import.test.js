d22e9ef1e3bfa4008a62b2166dc49a912321e361
James Sumners <git@zifbang.com>
2026-03-14 09:47:11 -0400

    Modernize code and drop old Node versions

--- test/import.test.js
+++ test/import.test.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const { test } = require('tap')
+const test = require('node:test')
 const { realImport } = require('../src')
 
 test('should use dynamic import', async (t) => {
   t.plan(1)
 
-  t.equal(await realImport('../src/index.js'), await import('../src/index.js'))
+  t.assert.equal(await realImport('../src/index.js'), await import('../src/index.js'))
 })
--- test/require.test.js
+++ test/require.test.js
@@ -1,22 +1,21 @@
 'use strict'
 
-const { test } = require('tap')
+const test = require('node:test')
 const { realRequire } = require('../src')
 
 test('should use webpack fallback require if available', (t) => {
   t.plan(1)
 
-  // eslint-disable-next-line camelcase
   globalThis.__non_webpack__require__ = function (arg) {
     return arg
   }
 
-  t.equal(realRequire('foobar'), 'foobar')
+  t.assert.equal(realRequire('foobar'), 'foobar')
 })
 
 test('should use regular require when nothing else is available', (t) => {
   t.plan(1)
 
   globalThis.__non_webpack__require__ = undefined
-  t.strictSame(require('../src'), realRequire('../src'))
+  t.assert.deepStrictEqual(require('../src'), realRequire('../src'))
 })
